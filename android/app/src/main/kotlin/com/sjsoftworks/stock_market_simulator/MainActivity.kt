d088d8a38271db2ed4d488bc509d2e402bd3cca9
swanand-op <81287332+swanand-op@users.noreply.github.com>
2022-10-22 11:45:12 +0530

    v0.1

---
+++ android/app/src/main/kotlin/com/sjsoftworks/stock_market_simulator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sjsoftworks.stock_market_simulator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MaterialApp(
+    home: MainWidget(),
+  ));
+}
+
+class MainWidget extends StatefulWidget {
+  const MainWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MainWidget> createState() => _MainWidgetState();
+}
+
+class _MainWidgetState extends State<MainWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
